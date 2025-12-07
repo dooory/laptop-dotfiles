083215bb656feeb7ea648c09ff7a7e811d68ac1e
dooory <78720364+dooory@users.noreply.github.com>
2025-12-08 09:17:55 +1300

    add setting opts and mappings

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,1 +1,2 @@
-print("Hello World!")
+require("opts")
+require("mappings")
---
+++ dot_config/nvim/lua/opts.lua
@@ -0,0 +1,9 @@
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.scrolloff = 999
+vim.opt.inccommand = "split"
+
+vim.opt.relativenumber = true
+vim.opt.number = true
