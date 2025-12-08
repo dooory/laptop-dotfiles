90b859372cfc5ab3606250e6b3343f63bd81ee4a
dooory <78720364+dooory@users.noreply.github.com>
2025-12-08 13:06:42 +1300

    add fzf and mappings

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -6,5 +6,37 @@ return {
 			'nvim-telescope/telescope-fzf-native.nvim', 
 			build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install' 
 		}
-	}
+	},
+	keys = {
+		{
+			"<leader>ff",
+			"<cmd>Telescope find_files<CR>",
+			desc = "Find Files"
+		},
+		{
+			"<leader>fh",
+			"<cmd>Telescope help_tags<CR>", 
+			desc = "Search help tags"
+		},		
+		{ 
+			"<leader>fg", 
+			"<cmd>Telescope live_grep<CR>", 
+			desc = "Live grep" 
+		},
+		{ 
+			"<leader>fc", 
+			function()
+				require("telescope.builtin").find_files({
+					cwd=vim.fn.stdpath("config"),
+					results_title="Found config files",
+				})
+			end, 
+			desc = "Find file in nvim config" 
+		},
+		{
+			"<C-p>",
+			"<cmd>Telescope git_files<CR>",
+			desc = "Search git files",
+		}
+	},
 }
