599e9c3acf65bd62770b03de767556bec239a1f9
dooory <78720364+dooory@users.noreply.github.com>
2025-12-09 14:19:59 +1300

    remove lsp progress notifications

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -1,11 +1,15 @@
 return {
 	'nvim-mini/mini.nvim',
 	version = false,
-	config =  function ()
+	config = function()
 		require("mini.animate").setup({})
 		require("mini.bracketed").setup({})
 		require("mini.git").setup({})
-		require("mini.notify").setup({})
+		require("mini.notify").setup({
+			lsp_progress = {
+				enabled = false
+			}
+		})
 		require("mini.pairs").setup({})
 		require("mini.statusline").setup({})
 		require("mini.surround").setup({})
