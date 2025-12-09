710a28430638117f74289deb535ffc479d1da776
dooory <78720364+dooory@users.noreply.github.com>
2025-12-09 13:34:32 +1300

    add chezmoi plugin

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,10 +1,11 @@
 return {
 	'nvim-telescope/telescope.nvim', tag = 'v0.2.0',
-	dependencies = { 
+	dependencies = {
 		'nvim-lua/plenary.nvim',
-		{ 
-			'nvim-telescope/telescope-fzf-native.nvim', 
-			build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install' 
+		'xvzc/chezmoi.nvim',
+		{
+			'nvim-telescope/telescope-fzf-native.nvim',
+			build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install'
 		}
 	},
 	keys = {
@@ -15,28 +16,51 @@ return {
 		},
 		{
 			"<leader>fh",
-			"<cmd>Telescope help_tags<CR>", 
+			"<cmd>Telescope help_tags<CR>",
 			desc = "Search help tags"
-		},		
-		{ 
-			"<leader>fg", 
-			"<cmd>Telescope live_grep<CR>", 
-			desc = "Live grep" 
 		},
-		{ 
-			"<leader>fc", 
+		{
+			"<leader>fg",
+			"<cmd>Telescope live_grep<CR>",
+			desc = "Live grep"
+		},
+		{
+			"<leader>fc",
 			function()
 				require("telescope.builtin").find_files({
 					cwd=vim.fn.stdpath("config"),
 					results_title="Found config files",
 				})
-			end, 
-			desc = "Find file in nvim config" 
+			end,
+			desc = "Find file in nvim config"
 		},
 		{
 			"<C-p>",
 			"<cmd>Telescope git_files<CR>",
 			desc = "Search git files",
-		}
+		},
+		{
+			"<leader>cz",
+			function ()
+				require("chezmoi.pick").telescope()
+			end,
+			desc = "Search all Chezmoi managed files",
+		},
+		{
+			"<leader>fc",
+			function ()
+				require("chezmoi.pick").telescope(
+					vim.fn.stdpath('config'),
+					{
+						"--path-style",
+						"absolute",
+						"--include",
+						"files",
+						"--exclude",
+						"externals",
+					}
+				)
+			end,
+		},
 	},
 }
