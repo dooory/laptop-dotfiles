2aaa3dafc7e474fd3cd6f1caa180225d8d8da14d
dooory <78720364+dooory@users.noreply.github.com>
2025-12-09 15:50:36 +1300

    remove search nvim config through chezmoi keymap

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,38 +1,39 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = 'v0.2.0',
+	"nvim-telescope/telescope.nvim",
+	tag = "v0.2.0",
 	dependencies = {
-		'nvim-lua/plenary.nvim',
-		'xvzc/chezmoi.nvim',
+		"nvim-lua/plenary.nvim",
+		"xvzc/chezmoi.nvim",
 		{
-			'nvim-telescope/telescope-fzf-native.nvim',
-			build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install'
-		}
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release --target install",
+		},
 	},
 	keys = {
 		{
 			"<leader>ff",
 			"<cmd>Telescope find_files<CR>",
-			desc = "Find Files"
+			desc = "Find Files",
 		},
 		{
 			"<leader>fh",
 			"<cmd>Telescope help_tags<CR>",
-			desc = "Search help tags"
+			desc = "Search help tags",
 		},
 		{
 			"<leader>fg",
 			"<cmd>Telescope live_grep<CR>",
-			desc = "Live grep"
+			desc = "Live grep",
 		},
 		{
 			"<leader>fc",
 			function()
 				require("telescope.builtin").find_files({
-					cwd=vim.fn.stdpath("config"),
-					results_title="Found config files",
+					cwd = vim.fn.stdpath("config"),
+					results_title = "Found config files",
 				})
 			end,
-			desc = "Find file in nvim config"
+			desc = "Find file in nvim config",
 		},
 		{
 			"<C-p>",
@@ -41,26 +42,10 @@ return {
 		},
 		{
 			"<leader>cz",
-			function ()
+			function()
 				require("chezmoi.pick").telescope()
 			end,
 			desc = "Search all Chezmoi managed files",
-		},
-		{
-			"<leader>fc",
-			function ()
-				require("chezmoi.pick").telescope(
-					vim.fn.stdpath('config'),
-					{
-						"--path-style",
-						"absolute",
-						"--include",
-						"files",
-						"--exclude",
-						"externals",
-					}
-				)
-			end,
 		},
 	},
 }
