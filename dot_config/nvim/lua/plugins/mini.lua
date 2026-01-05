3aa475c63f835bbb0b355a5d8746c862c608cab1
dooory <78720364+dooory@users.noreply.github.com>
2026-01-06 12:05:44 +1300

    remove mini.animate plugin

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -1,17 +1,16 @@
 return {
-	'nvim-mini/mini.nvim',
+	"nvim-mini/mini.nvim",
 	version = false,
 	config = function()
-		require("mini.animate").setup({})
 		require("mini.bracketed").setup({})
 		require("mini.git").setup({})
 		require("mini.notify").setup({
 			lsp_progress = {
-				enable = false
-			}
+				enable = false,
+			},
 		})
 		require("mini.pairs").setup({})
 		require("mini.statusline").setup({})
 		require("mini.surround").setup({})
-	end
+	end,
 }
