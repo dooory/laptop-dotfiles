378a4ad61489badbd7aab44bdfc7290d07046f16
dooory <78720364+dooory@users.noreply.github.com>
2026-01-07 09:24:17 +1300

    remove mini.notify

--- dot_config/nvim/lua/plugins/mini.lua
+++ dot_config/nvim/lua/plugins/mini.lua
@@ -4,11 +4,6 @@ return {
 	config = function()
 		require("mini.bracketed").setup({})
 		require("mini.git").setup({})
-		require("mini.notify").setup({
-			lsp_progress = {
-				enable = false,
-			},
-		})
 		require("mini.pairs").setup({})
 		require("mini.statusline").setup({})
 		require("mini.surround").setup({})
