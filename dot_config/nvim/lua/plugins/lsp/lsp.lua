3c86aa9184d9b098e8a20ce92aae3460a18c1655
dooory <78720364+dooory@users.noreply.github.com>
2026-01-13 12:31:23 +1300

    add biome formatter

--- dot_config/nvim/lua/plugins/lsp/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp/lsp.lua
@@ -1,19 +1,25 @@
 return {
 	"neovim/nvim-lspconfig",
 	dependencies = {
-		'saghen/blink.cmp',
-		'saghen/blink.indent',
+		"saghen/blink.cmp",
+		"saghen/blink.indent",
 	},
 
 	opts = {
 		servers = {
-			lua_ls = {}
-		}
+			lua_ls = {},
+			biome = {
+				formatter = {
+					indentStyle = "space",
+					indentWidth = 4,
+				},
+			},
+		},
 	},
 
 	config = function(_, opts)
 		for server, config in pairs(opts.servers) do
-			config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+			config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
 			vim.lsp.config[server] = config
 		end
 	end,
